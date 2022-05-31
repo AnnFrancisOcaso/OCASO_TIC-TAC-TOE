5e8f0972c25b1006c818d9cbeaf4d0a1885842d5
Ann Francis Ocaso <ocasoannfrancis100202@gmail.com>
2022-05-31 15:59:33 +0800

    inserting the buttons and renaming the form1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,130 @@ namespace ocaso_tic_tac_toe
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.R1 = new System.Windows.Forms.Button();
+            this.R2 = new System.Windows.Forms.Button();
+            this.R3 = new System.Windows.Forms.Button();
+            this.S3 = new System.Windows.Forms.Button();
+            this.S2 = new System.Windows.Forms.Button();
+            this.S1 = new System.Windows.Forms.Button();
+            this.T3 = new System.Windows.Forms.Button();
+            this.T2 = new System.Windows.Forms.Button();
+            this.T1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // R1
+            // 
+            this.R1.Font = new System.Drawing.Font("Segoe UI Historic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R1.Location = new System.Drawing.Point(23, 79);
+            this.R1.Name = "R1";
+            this.R1.Size = new System.Drawing.Size(80, 80);
+            this.R1.TabIndex = 0;
+            this.R1.UseVisualStyleBackColor = true;
+            // 
+            // R2
+            // 
+            this.R2.Font = new System.Drawing.Font("Segoe UI Historic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R2.Location = new System.Drawing.Point(110, 79);
+            this.R2.Name = "R2";
+            this.R2.Size = new System.Drawing.Size(80, 80);
+            this.R2.TabIndex = 1;
+            this.R2.UseVisualStyleBackColor = true;
+            // 
+            // R3
+            // 
+            this.R3.Font = new System.Drawing.Font("Segoe UI Historic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R3.Location = new System.Drawing.Point(199, 79);
+            this.R3.Name = "R3";
+            this.R3.Size = new System.Drawing.Size(80, 80);
+            this.R3.TabIndex = 2;
+            this.R3.UseVisualStyleBackColor = true;
+            // 
+            // S3
+            // 
+            this.S3.Font = new System.Drawing.Font("Segoe UI Historic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.S3.Location = new System.Drawing.Point(199, 165);
+            this.S3.Name = "S3";
+            this.S3.Size = new System.Drawing.Size(80, 80);
+            this.S3.TabIndex = 5;
+            this.S3.UseVisualStyleBackColor = true;
+            // 
+            // S2
+            // 
+            this.S2.Font = new System.Drawing.Font("Segoe UI Historic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.S2.Location = new System.Drawing.Point(110, 165);
+            this.S2.Name = "S2";
+            this.S2.Size = new System.Drawing.Size(80, 80);
+            this.S2.TabIndex = 4;
+            this.S2.UseVisualStyleBackColor = true;
+            // 
+            // S1
+            // 
+            this.S1.Font = new System.Drawing.Font("Segoe UI Historic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.S1.Location = new System.Drawing.Point(23, 165);
+            this.S1.Name = "S1";
+            this.S1.Size = new System.Drawing.Size(80, 80);
+            this.S1.TabIndex = 3;
+            this.S1.UseVisualStyleBackColor = true;
+            // 
+            // T3
+            // 
+            this.T3.Font = new System.Drawing.Font("Segoe UI Historic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.T3.Location = new System.Drawing.Point(199, 251);
+            this.T3.Name = "T3";
+            this.T3.Size = new System.Drawing.Size(80, 80);
+            this.T3.TabIndex = 8;
+            this.T3.UseVisualStyleBackColor = true;
+            // 
+            // T2
+            // 
+            this.T2.Font = new System.Drawing.Font("Segoe UI Historic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.T2.Location = new System.Drawing.Point(110, 251);
+            this.T2.Name = "T2";
+            this.T2.Size = new System.Drawing.Size(80, 80);
+            this.T2.TabIndex = 7;
+            this.T2.UseVisualStyleBackColor = true;
+            // 
+            // T1
+            // 
+            this.T1.Font = new System.Drawing.Font("Segoe UI Historic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.T1.Location = new System.Drawing.Point(23, 251);
+            this.T1.Name = "T1";
+            this.T1.Size = new System.Drawing.Size(80, 80);
+            this.T1.TabIndex = 6;
+            this.T1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(300, 351);
+            this.Controls.Add(this.T3);
+            this.Controls.Add(this.T2);
+            this.Controls.Add(this.T1);
+            this.Controls.Add(this.S3);
+            this.Controls.Add(this.S2);
+            this.Controls.Add(this.S1);
+            this.Controls.Add(this.R3);
+            this.Controls.Add(this.R2);
+            this.Controls.Add(this.R1);
+            this.Name = "Form1";
+            this.Text = "TIC-TAC-TOE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button R1;
+        private System.Windows.Forms.Button R2;
+        private System.Windows.Forms.Button R3;
+        private System.Windows.Forms.Button S3;
+        private System.Windows.Forms.Button S2;
+        private System.Windows.Forms.Button S1;
+        private System.Windows.Forms.Button T3;
+        private System.Windows.Forms.Button T2;
+        private System.Windows.Forms.Button T1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace ocaso_tic_tac_toe
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
